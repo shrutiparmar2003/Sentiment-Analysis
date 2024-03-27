83992cc87aef51d0341ed4552aa8ee93de09f814
shrutiparmar2003 <shrutiparmar01082003@gmail.com>
2024-03-27 15:22:38 +0530

    first commit

---
+++ sentiment_analysis.py
@@ -0,0 +1,65 @@
+import string
+
+from collections import Counter
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+
+
+text = open('read.txt', encoding='utf-8').read()
+
+lower_case = text.lower()
+cleaned_txt = lower_case.translate(str.maketrans('', '', string.punctuation))
+
+tokenized_words = word_tokenize(cleaned_txt, "english")
+# creates list of all the words separated by comma
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+final_word = []
+for word in tokenized_words:
+    if word not in stopwords.words("english"):
+        final_word.append(word)
+
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n', '').replace(',', '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_word:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+count = Counter(emotion_list)
+print(count)
+
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    if score['neg'] > score['pos']:
+        print("Negative Sentiment")
+    elif score['neg'] < score['pos']:
+        print("Positive Sentiment")
+    else:
+        print("Neutral Sentiment")
+
+
+sentiment_analyse(cleaned_txt)
+
+plt.bar(count.keys(), count.values(), color='coral', width=0.4)
+plt.title('Sentiment analysis')
+plt.xlabel('Emotion')
+plt.ylabel('No. of times appeared in text')
+
+plt.savefig('graph.png')
+plt.show()
---
+++ twitter_analysis.py
@@ -0,0 +1,70 @@
+
+import string
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+import GetOldTweets3 as got
+
+
+def get_tweets():
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('corona virus') \
+        .setSince("2020-01-01") \
+        .setUntil("2021-09-30") \
+        .setMaxTweets(10)
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)[tweetCriteria]
+
+    text_tweets = [[tweet.text] for tweet in tweets]
+    return text_tweet
+
+
+text = ""
+text_tweet = get_tweets()
+length = len(text_tweet)
+for i in range(0, length):
+    text = text_tweet[i][0] + " " + text
+
+text = open('read.txt', encoding='utf-8').read()
+
+lower_case = text.lower()
+cleaned_txt = lower_case.translate(str.maketrans('','',string.punctuation))
+
+tokenized_words = word_tokenize(cleaned_txt, "english")# creates list of all the words separated by comma
+
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+final_word = []
+for word in tokenized_words:
+    if word not in stopwords.words("english"):
+        final_word.append(word)
+
+emotion_list = []
+with open('emotions.txt', 'r') as file:
+    for line in file:
+        clear_line = line.replace('\n','').replace(',','').replace("'",'').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_word:
+            emotion_list.append(emotion)
+
+print(emotion_list)
+count = Counter(emotion_list)
+print(count)
+
+
+plt.bar(count.keys() , count.values(),color='coral',width=0.4)
+plt.title('Sentiment analysis')
+plt.xlabel('Emotion')
+plt.ylabel('No. of times appeared in text')
+
+plt.savefig('graph.png')
+plt.show()
+
